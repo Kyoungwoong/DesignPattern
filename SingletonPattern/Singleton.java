896aaf619b03313ca1b3b8649942057079a7ceee
Kyoungwoong <kyy980708@gmail.com>
2024-01-12 09:39:29 +0900

    synchronized 키워드를 사용한 멀티스레딩 문제 해결

--- SingletonPattern/Singleton.java
+++ SingletonPattern/Singleton.java
@@ -1,5 +1,5 @@
 public class Singleton {
-    private static Singleton singleton; // Singleton 클래스의 하나뿐인 인스턴스를 저장하는 전역변수
+    private static Singleton singleton; // Singleton 클래스의 하나뿐인 인스턴스를 저장하는 정적변수
 
     private Singleton() {} // 생성자를 private로 선언했으므 Singleton 클래스에서만 생성자 생성 가능
 
