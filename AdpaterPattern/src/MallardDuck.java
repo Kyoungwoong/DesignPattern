af9f5b917cc314203f0abd49d8a4b4f2b1b2f220
Kyoungwoong <kyy980708@gmail.com>
2024-01-22 16:38:34 +0900

    간단한 어댑터패턴

---
+++ AdpaterPattern/src/MallardDuck.java
@@ -0,0 +1,11 @@
+public class MallardDuck implements Duck{
+    @Override
+    public void quack() {
+        System.out.println("꽥");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("i can fly");
+    }
+}
