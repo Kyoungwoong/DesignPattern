5418b4c7564e268c01043ca4438d1b2fa880f9a8
Kyoungwoong <kyy980708@gmail.com>
2024-01-29 18:51:37 +0900

    템플릿 메소드 적용

--- TemplateMethodPattern/src/Coffee.java
+++ TemplateMethodPattern/src/Coffee.java
@@ -1,25 +1,12 @@
-public class Coffee {
+public class Coffee extends CaffeineBeverage{
 
-    void prepareRecipe() {
-        boilWater();
-        brewCoffeeGrinds();
-        pourInCup();
-        addSugarAndMilk();
-    }
-
-    public void boilWater() {
-        System.out.println("물 끓이는 중");
-    }
-
-    public void brewCoffeeGrinds() {
+    @Override
+    public void brew() {
         System.out.println("필터로 커피 우려내는 중");
     }
 
-    public void pourInCup() {
-        System.out.println("컵에 따르는 중");
-    }
-
-    public void addSugarAndMilk() {
+    @Override
+    public void addCondiments() {
         System.out.println("설탕과 우유를 추가하는 중");
     }
 }
