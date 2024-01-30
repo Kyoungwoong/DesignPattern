88ac1b2346dc805b5e093ad134571bcdd7a70d91
Kyoungwoong <kyy980708@gmail.com>
2024-01-30 12:47:11 +0900

    종업원 코드 리팩토링

---
+++ IteratorPattern/src/transition/MenuTestDrive.java
@@ -0,0 +1,19 @@
+package transition;
+import java.util.ArrayList;
+
+public class MenuTestDrive {
+	public static void main(String args[]) {
+		ArrayList<Menu> menus = new ArrayList<Menu>();
+
+		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+		DinerMenu dinerMenu = new DinerMenu();
+
+		menus.add(pancakeHouseMenu);
+		menus.add(dinerMenu);
+
+		Waitress waitress = new Waitress(menus);
+		
+		waitress.printMenu();
+
+	}
+}
