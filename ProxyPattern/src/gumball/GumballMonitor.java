fbf57f28fa50616f24f7108acb0e51a180256f95
Kyoungwoong <kyy980708@gmail.com>
2024-02-01 15:12:21 +0900

    원격 프록시 RMI 적용

---
+++ ProxyPattern/src/gumball/GumballMonitor.java
@@ -0,0 +1,20 @@
+package gumball;
+
+public class GumballMonitor {
+    GumballMachineRemote gumballMachine;
+
+    public GumballMonitor(GumballMachineRemote gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void report() {
+
+        try {
+            System.out.println("뽑기 기계 위치: " + gumballMachine.getLocation());
+            System.out.println("현재 재고: " + gumballMachine.getCount() + " 개");
+            System.out.println("현재 상태: " + gumballMachine.getState());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
